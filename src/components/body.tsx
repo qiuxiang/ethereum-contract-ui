af4d22364482471717d67306913900378334e95b
qiuxiang <i@7c00.cc>
2022-04-09 13:03:25 +0000

    Deploying to gh-pages from @ qiuxiang/ethereum-contract-ui@39cfe0cdbc53e609506935eb327ecb34594d9f43 🚀

--- src/components/body.tsx
+++
@@ -1,9 +0,0 @@
-import { Layout } from "antd";
-import { BasicProps } from "antd/lib/layout/layout";
-import * as React from "react";
-
-export const Body = ({ children }: BasicProps) => (
-  <Layout.Content style={{ padding: "0 24px 24px 24px" }}>
-    {children}
-  </Layout.Content>
-);
